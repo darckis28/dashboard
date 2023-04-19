4115548db571493277fb91bc9588f70daa73283c
darckis <aguirre280912@gmail.com>
2023-04-19 14:23:28 -0500

    agregando styles en navbar

--- src/components/shared/sidebar.jsx
+++ src/components/shared/sidebar.jsx
@@ -24,35 +24,46 @@ const StyledContainer = styled.nav`
   justify-content: space-between;
 `;
 const ContenedorItems = styled.ul`
-  margin:20px 0;
-  display:flex;
-  flex-direction:column;
-  gap:2px;
-  padding-left:10px;
-  .logo{
-    background-color:#1f1c2a;
+  margin: 20px 0;
+  display: flex;
+  flex-direction: column;
+  gap: 2px;
+  padding-left: 10px;
+  .active {
+    background-color: #262736;
+  }
+  .active a {
+    background-color: #e87969;
+    color: white;
   }
 `;
 const Logo = styled.img`
   width: 50px;
 `;
+const ItemLogo = styled.li`
+  display: flex;
+  justify-content: center;
+  margin-bottom:10px;
+`;
 const Items = styled.li`
   list-style: none;
-  background-color: #262736;
   display: flex;
   justify-content: center;
   padding: 14px;
-  font-size:18px;
-  border-top-left-radius:5px;
-  border-bottom-left-radius:5px;
-  a{
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    color:white;
-    background-color:#E87969;
-    padding:8px 12px;
-    border-radius:5px;
+  font-size: 18px;
+  border-top-left-radius: 5px;
+  border-bottom-left-radius: 5px;
+  transition: all .5s ease;
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 8px 12px;
+    border-radius: 5px;
+    color: #e87969;
+  }
+  :hover {
+    background-color: #262736;
   }
 `;
 
@@ -60,38 +71,49 @@ export const SideBar = () => {
   return (
     <StyledContainer>
       <ContenedorItems>
-        <Items className="logo">
+        <ItemLogo>
           <Logo src={logo} alt="logo-dashboard" />
+        </ItemLogo>
+        <Items className="active">
+          <a href="#">
+            {" "}
+            <RiHome3Line />
+          </a>
         </Items>
         <Items>
-          <a href="#"> <RiHome3Line /></a>
-         
+          <a href="#">
+            <RiPercentLine />
+          </a>
         </Items>
         <Items>
-          <a href="#"><RiPercentLine /></a>
-          
+          <a href="#">
+            {" "}
+            <RiPieChartLine />
+          </a>
         </Items>
         <Items>
-          <a href="#"> <RiPieChartLine /></a>
-         
+          <a href="#">
+            <RiMailLine />
+          </a>
         </Items>
         <Items>
-        <a href="#"><RiMailLine /></a>
-          
+          <a href="#">
+            <RiNotification4Line />
+          </a>
         </Items>
         <Items>
-        <a href="#"><RiNotification4Line /></a>
-          
-        </Items>
-        <Items>
-        <a href="#"> <RiSettings4Line /></a>
-         
+          <a href="#">
+            {" "}
+            <RiSettings4Line />
+          </a>
         </Items>
       </ContenedorItems>
       <ContenedorItems>
         <Items>
-        <a href="#"> <RiLogoutCircleLine /></a>
-         
+          <a href="#">
+            {" "}
+            <RiLogoutCircleLine />
+          </a>
         </Items>
       </ContenedorItems>
     </StyledContainer>
